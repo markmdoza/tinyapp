1b6bfc5f3aa2d1c647f6678f9773f633bf57f42d
Mark Mendoza <marklmdoza@gmail.com>
2023-07-23 23:46:16 -0400

    Fill out the urls_index.ejs template

--- express_server.js
+++ express_server.js
@@ -2,20 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
-
-app.set('view engine', 'ejs');
 
 app.get('/', (req,res) => {
   res.send('Hello!');
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+  const urlData = { urls: urlDatabase};
+  res.render('urls_index', urlData);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: 'www.lighthouselabs.ca' };
+  res.render("urls_show", templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
