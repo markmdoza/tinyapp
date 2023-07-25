62289a88bcd6e5e14193ec0bfb181a97282e6dd5
Mark Mendoza <marklmdoza@gmail.com>
2023-07-25 16:49:31 -0400

    Add a second route template and test the route

--- express_server.js
+++ express_server.js
@@ -19,7 +19,10 @@ app.get('/urls', (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: 'www.lighthouselabs.ca' };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id] 
+  };
   res.render("urls_show", templateVars);
 });
 
